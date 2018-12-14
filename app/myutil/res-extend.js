23fd15a5bb58da9205390afe66299f9f3707e8bd
morehao <1165510913@qq.com>
2018-12-15 00:30:07 +0800

    feat(logs): 日志修改，增加返回数据的日志

--- app/myutil/res-extend.js
+++ app/myutil/res-extend.js
@@ -1,5 +1,7 @@
 'use strict'
 const lodash = require('lodash')
+const log4js = require('log4js')
+const logger = log4js.getLogger('cheese')
 
 module.exports = (req, res, next) => {
   const extendAttr = {
@@ -9,9 +11,11 @@ module.exports = (req, res, next) => {
         errorCode: 0,
         data: data
       }
+      logger.info(`sendOk:${JSON.stringify(data)}`)
       return res.send(lodash.extend(rst))
     },
     sendErr: (errorInfo) => {
+      logger.info(`sendErr:${JSON.stringify(errorInfo)}`)
       return res.send(lodash.extend(errorInfo))
     }
   }
--- server.js
+++ server.js
@@ -5,14 +5,23 @@ const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const log4js = require('log4js')
 
 const {settings} = require('./config')
 const myutil = require('./app/myutil')
+
 const routes = require('./app/routes/routes') // 引入路由
 
 // 日志配置
-const {log} = require('./app/middlewares')
-app.all('*', log)
+log4js.configure('./config/log4js.json')
+const logger = log4js.getLogger('cheese')
+// app.use(log4js.connectLogger(logger, {level: 'info'}))
+// app.use(log4js.connectLogger(logger, { level: log4js.levels.INFO, format: ':method :url' }))
+app.use(log4js.connectLogger(logger, {
+  level: 'auto',
+  // include the Express request ID in the logs
+  format: (req, res, format) => format(`:remote-addr - ${req.id} - [time]${new Date()} [method]":method" - [url]":url" - [status]:status - [content-length]:content-length - [request-body]${JSON.stringify(req.body)}`)
+}))
 // 配置静态文件
 app.use(express.static(path.join(__dirname, 'app/public')))
 // 配置apidoc
@@ -36,5 +45,6 @@ app.use(cors())
 routes(app)
 app.listen(settings.port)
 console.log('express-blog server started on: ' + settings.port)
+logger.info(`port is ${settings.port}`)
 
 module.exports = app
